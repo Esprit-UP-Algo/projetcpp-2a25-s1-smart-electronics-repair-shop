5e8e7dc2d9cf3047e4d103b5c568a9804a7f9e99
mouna2240 <mouna.ncib@esprit.tn>
2025-10-05 18:53:59 +0100

    premier commit projet qt interfacemouna

---
+++ interfacemouna/build/Desktop_Qt_6_7_3_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_interfacemouna_FILE C:/Users/mouna/OneDrive/2eme projet/interfacemouna/build/Desktop_Qt_6_7_3_llvm_mingw_64_bit-Debug/interfacemouna.exe)
+set(__QT_DEPLOY_TARGET_interfacemouna_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_interfacemouna_RUNTIME_DLLS C:/Qt/6.7.3/llvm-mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.3/llvm-mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.3/llvm-mingw_64/bin/Qt6Core.dll)
